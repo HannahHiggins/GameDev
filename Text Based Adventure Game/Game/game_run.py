09fd467143f86ae0c0d091b6d75fe5bf2cfaed5c
HannahHiggins <meowhannah@hotmail.co.uk>
2015-03-04 22:34:16 +0000

    Added run class

--- Text Based Adventure Game/Game/game_run.py
+++ Text Based Adventure Game/Game/game_run.py
@@ -3,38 +3,53 @@ import character
 import world
 import random
 import inventory
+import movement
 
-def run_sequence():
-    print("Welcome to the game")
-    name = raw_input("What is your name?")
-    print("Hello "+ name)
-    char_class = raw_input("Choose a class")
+class run():
+    def __init__(self):
+        print("Welcome to the game")
+        name = raw_input("What is your name?")
+        print("Hello "+ name)
+        char_class = raw_input("Choose a class")
     
-    #initialise character
-    pchar = character.Character(name, char_class)
+        #initialise character
+        self.pchar = character.Character(name, char_class)
     
-    print(name + " the " + char_class)
+        print(name + " the " + char_class)
     
-    if (char_class == "Mage"):
-        print("A good choice!")
-    if (char_class == "Hunter"):
-        print("A great choice!")
+        if (char_class == "Mage"):
+            print("A good choice!")
+        if (char_class == "Hunter"):
+            print("A great choice!")
+        else:
+            print("Not a valid response (Mage or Hunter)")
 
-    print("Let us begin...")
+        print("Let us begin...")
+        
+        #initialise world
+        self.pworld = world.World(random.randint(1,10))
+        #initialise inventory
+        self.pinventory = inventory.Inventory()
+        
+        print("Welcome to "+ self.pworld.world_name + ", a " + self.pworld.world_type + " world.")
+        
+        #scripted - for now
+        new_item = "helm"
+        
+        print(self.pinventory.add_item(new_item))
+        
+        #test
+        print("You discovered a new area - gained 500 exp")
+        self.pchar.add_exp(500)
     
-    #initialise world
-    pworld = world.World(random.randint(1,10))
-    #initialise inventory
-    pinventory = inventory.Inventory()
+        self.main_run()
     
-    print("Welcome to "+ pworld.world_name + ", a " + pworld.world_type + " world.")
-    
-    #scripted - for now
-    new_item = "helm"
-    
-    print(pinventory.add_item(new_item))
-    
-    
+    def main_run(self):
+        command = raw_input("Where do you want to go?")
+        x_change, y_change = movement.movement(command)
+        self.pchar.change_location(x_change, y_change)
+        #calls itself
+        self.main_run()
         
 if __name__ == "__main__":
-    run_sequence()
+    run()
--- Text Based Adventure Game/game/game_run.py
+++ Text Based Adventure Game/game/game_run.py
@@ -5,51 +5,51 @@ import random
 import inventory
 import movement
 
+class run():
+    def __init__(self):
+        print("Welcome to the game")
+        name = raw_input("What is your name?")
+        print("Hello "+ name)
+        char_class = raw_input("Choose a class")
+    
+        #initialise character
+        self.pchar = character.Character(name, char_class)
+    
+        print(name + " the " + char_class)
+    
+        if (char_class == "Mage"):
+            print("A good choice!")
+        if (char_class == "Hunter"):
+            print("A great choice!")
+        else:
+            print("Not a valid response (Mage or Hunter)")
 
-def intro():
-    print("Welcome to the game")
-    name = raw_input("What is your name?")
-    print("Hello "+ name)
-    char_class = raw_input("Choose a class")
+        print("Let us begin...")
+        
+        #initialise world
+        self.pworld = world.World(random.randint(1,10))
+        #initialise inventory
+        self.pinventory = inventory.Inventory()
+        
+        print("Welcome to "+ self.pworld.world_name + ", a " + self.pworld.world_type + " world.")
+        
+        #scripted - for now
+        new_item = "helm"
+        
+        print(self.pinventory.add_item(new_item))
+        
+        #test
+        print("You discovered a new area - gained 500 exp")
+        self.pchar.add_exp(500)
     
-    #initialise character
-    pchar = character.Character(name, char_class)
+        self.main_run()
     
-    print(name + " the " + char_class)
-    
-    if (char_class == "Mage"):
-        print("A good choice!")
-    if (char_class == "Hunter"):
-        print("A great choice!")
-    else:
-        print("Not a valid response (Mage or Hunter)")
-
-    print("Let us begin...")
-    
-    #initialise world
-    pworld = world.World(random.randint(1,10))
-    #initialise inventory
-    pinventory = inventory.Inventory()
-    
-    print("Welcome to "+ pworld.world_name + ", a " + pworld.world_type + " world.")
-    
-    #scripted - for now
-    new_item = "helm"
-    
-    print(pinventory.add_item(new_item))
-    
-    #test
-    print("You discovered a new area - gained 500 exp")
-    pchar.add_exp(500)
-    
-    main_run(pchar)
-    
-def main_run(pchar):
-    command = raw_input("Where do you want to go?")
-    x_change, y_change = movement.movement(command)
-    pchar.change_location(x_change, y_change)
-    #calls itself
-    main_run(pchar)
+    def main_run(self):
+        command = raw_input("Where do you want to go?")
+        x_change, y_change = movement.movement(command)
+        self.pchar.change_location(x_change, y_change)
+        #calls itself
+        self.main_run()
         
 if __name__ == "__main__":
-    intro()
+    run()
